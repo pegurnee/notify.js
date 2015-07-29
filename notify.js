b74132858750ea789996082a3d9f94e8ea9de70a
Eddie Gurnee <pegurnee@gmail.com>
2015-07-29 14:56:59 -0400

    fixed location of notification

--- notify.js
+++ notify.js
@@ -1,18 +1,18 @@
 var notify = (function (document) {
     "use strict";
     var notifySpace, notify, theBackgroundColor;
-    
+
     notify = function (message, type) {
         if (!document.getElementById('notify-location')) {
             notifySpace = document.createElement('div');
             notifySpace.id = 'notify-location';
-            notifySpace.style.top = '0px';
-            notifySpace.style.right = '0px';
-            notifySpace.style.position = 'absolute';
+            notifySpace.style.top = '10px';
+            notifySpace.style.right = '10px';
+            notifySpace.style.position = 'fixed';
 
             document.body.appendChild(notifySpace);
         }
-        
+
         switch (type) {
         case 'failure':
             theBackgroundColor = 'rgba(200, 0, 25, .3)';
@@ -23,15 +23,16 @@ var notify = (function (document) {
         default:
             theBackgroundColor = 'rgba(100, 100, 25, .3)';
         }
-        
+
         var inner = document.createElement('div');
         inner.innerHTML = message;
-        
+
         inner.style.backgroundColor =  theBackgroundColor;
         inner.style.padding = '2px';
         inner.style.border = '1px black solid';
         inner.style.borderRadius = '3px';
-        
+        inner.style.margin = '2px';
+
         notifySpace.appendChild(inner);
         setTimeout(function () {notifySpace.removeChild(inner); }, 1500);
     };
